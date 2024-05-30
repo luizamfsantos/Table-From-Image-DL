a3e65cc1d295893e9f20c9f67a1c2758935bad03
luizamfsantos <luiza.marques_@hotmail.com>
2024-05-30 19:01:26 -0300

    feat: add to main ingestion step

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from src.tableDecoder import logger
-import src.tableDecoder.utils.common
+from src.tableDecoder.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Welcome to our custom log')
+
+STAGE_NAME = 'Data Ingestion stage'
+try:
+    logger.info(f'>>>>>>>>>>>> Starting {STAGE_NAME} <<<<<<<<<<<<')
+    obj = DataIngestionTrainingPipeline()
+    obj.run()
+    logger.info(f'>>>>>>>>>>>> Completed {STAGE_NAME} <<<<<<<<<<<<')
+except Exception as e:
+    logger.error(f'Error in {STAGE_NAME}')
+    raise e
